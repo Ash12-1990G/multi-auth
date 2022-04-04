8913ee06c0619b569a00bc241367e4723fcb7a21
Ash12-1990G <ashig9020@gmail.com>
2022-04-04 16:42:37 +0530

    role

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -46,7 +46,7 @@
                                 <td>{{ $role->name }}</td>
                                 <td>
                                     <a class="btn btn-success btn-sm" href="{{ route('roles.show',$role->id) }}">Show</a>
-                                    @if($role->name == 'super-admin' || $role->name == 'Franchise-Admin' || $role->name == 'Student-Admin')
+                                    @if($role->name !== 'super-admin' || $role->name !== 'Franchise-Admin' || $role->name !== 'Student-Admin')
                                     @can('role-edit')
                                         <a class="btn btn-primary btn-sm" href="{{ route('roles.edit',$role->id) }}">Edit</a>
                                     @endcan
